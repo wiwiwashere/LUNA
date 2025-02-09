f87e682413bdd374f42cca5b570d0c525cdceda2
wiwiwashere <winnielin56@gmail.com>
2025-02-09 00:08:38 -0500

    splash

--- front_end/app/_layout.tsx
+++ front_end/app/_layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="splash" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
         <Stack.Screen name="results" options={{ title: "Results" }} />
